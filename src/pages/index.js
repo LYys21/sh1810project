b4d15c895a657e1a244e33b81a81f55c81f781cc
yintao <875886087@qq.com>
2018-12-06 11:22:25 +0800

    修改了什么东西

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+
+require(["../scripts/config.js"], function(){
+	require(["common","jquery", "swiper","a", "fontscroll"], function(com, $, Swiper, a ){
+		
+		$(function(){
+			
+		})
+		
+		com.randomColor();
+		
+		// randomColor();
+		
+		console.log("index ok");
+		
+	})
+})
+
